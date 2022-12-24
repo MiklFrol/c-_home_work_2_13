64f115890a2cdcf351dc70d809adde2a56c58551
MiklFrol <miklfrol@mail.ru>
2022-12-24 23:54:53 +0300

    Задача 13 выполнена

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа принимает число, и выводит третью цифру введенного числа
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+
+int counterDigits = 0;
+int tempNumber = number;
+int thirdDigit = 0;
+
+if (number < 100)
+{
+    Console.WriteLine("Третья цифра отсутствует!");
+}
+else
+{
+    while (tempNumber > 0)
+{
+    counterDigits++;
+    tempNumber = tempNumber / 10;
+}
+
+while (counterDigits > 3)
+{
+    number = number / 10;
+    counterDigits = counterDigits - 1;
+}
+
+thirdDigit = number % 10;
+
+Console.WriteLine($"Третья цифра введенного цисла - {thirdDigit}");
+}
